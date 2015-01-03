c48cdf1dbf79e22269e14fe2c66519c6e4617aa4
Danny Song <danny.song.ga@gmail.com>
2015-01-03 17:43:44 +0900

    clocksync : add comment

--- 4.algospot_clocksync/algospot_clocksync.sedman.cpp
+++ 4.algospot_clocksync/algospot_clocksync.sedman.cpp
@@ -4,18 +4,33 @@
 #include <algorithm>
 #include <limits>
 
+
+/* TODO : no need to use TEXTBOOK define
+*/
+
+/* [brief]
+ * clocksync 
+ * https://algospot.com/judge/problem/read/CLOCKSYNC
+ * performance : 2877ms
+ * etc : c++11
+ */
+
 //#define DEBUG
+//#define TEXTBOOK
 
 class clocksync
 {
 private:
 	clocksync(const clocksync&) = delete;
-	clocksync& operator(const clocksync& ) = delete;
+	clocksync& operator=(const clocksync& ) = delete;
 
+protected:
 	typedef std::array<std::size_t, 16> clocks_t;
 	clocks_t clocks;
 	typedef std::array<int, 5> clocksPerSwitch_t;
-	std::array<clocksPerSwitch_t, 10> switches = {{
+	
+	static const std::size_t nSwitch = 10;
+	std::array<clocksPerSwitch_t, nSwitch> switches = {{
 	{{0,  1,  2, -1, -1}},
 	{{3,  7,  9, 11, -1}},
 	{{4, 10, 14, 15, -1}},
@@ -31,6 +46,7 @@ private:
 	std::size_t MAX = std::numeric_limits<std::size_t>::max();
 	std::size_t pressed = 0;
 	std::size_t minPressed = MAX;
+	static const int HIT = 0;
 
 	bool allReachAt12(void)
 	{
@@ -63,53 +79,48 @@ private:
 		}
 	}
 
-	std::size_t search(const std::size_t button)
+	virtual std::size_t search(const std::size_t cur_switch)
 	{
 #ifdef DEBUG
-		std::cout<<"\n+++++ new seach button : "<<button<<" pressed : "<<pressed<<std::endl;
+		std::cout<<"\n+++++ new seach cur_switch : "<<cur_switch<<" pressed : "<<pressed<<std::endl;
 #endif
 		//hit!!!
 		if(allReachAt12() == true)
 		{
 			print("******allReachAt12");	
-			return 0;
+			return HIT;
 		}
 
-		//Can't go further. Only available from 0 to 9
-		if(button == 10)
+		//Can't go further.
+		if(cur_switch == nSwitch)
 		{
-			print("------button reaches to 10");	
+			print("------cur_switch reaches to max. nSwitch");	
 			return MAX;
 		}
 
-		//from 0 to 3
+		//0 ~ 2 : press
+		//3 : no press 
 		std::size_t idx = 0;
 		for(; idx < 4; ++idx)
 		{
-			std::size_t ret = search(button + 1);
+			std::size_t ret = search(cur_switch + 1);
 #ifdef DEBUG
 			std::cout<<"minPressed : "<<minPressed<<" ret : "<<ret<<" idx :"<<idx<<"\n";
 #endif
-			if(ret == 0 && minPressed > pressed)
+			if(ret == HIT && minPressed > pressed)
 			{
-#ifdef DEBUG
-				std::cout<<"#####hit : "<<pressed<<std::endl;
-#endif
 				minPressed = pressed;
 			}
 
 			++pressed;
 
-			pressSwitch(button);
+			pressSwitch(cur_switch);
 			print_clocks("");
 		}
 
+		//restore, bacause clock returns to its original point.
 		pressed -= 4;
-
 		
-#ifdef DEBUG
-		std::cout<<"-------button : "<<button<<" pressed : "<<pressed<<"\n\n";
-#endif
 		return minPressed;
 	}
 
@@ -136,31 +147,56 @@ public:
 
 	int calc(void)
 	{
+		//init
 		pressed = 0;
 		minPressed = MAX;
+		
+		//prepare input
 		for(auto &item : clocks)
 		{
 			std::cin>>item;
 		}
 
-#ifdef DEBUG
-		for(auto item : clocks)
-		{
-			std::cout<<item<<" ";
-		}
-#endif
+		print_clocks("");
+	
+		//exception case
 		if(allReachAt12() == true)
 		{
 			return 1;
 		}
 		
-
+#ifdef TEXTBOOK
+		return search(0);
+#else
 		search(0);
-		
 		return (minPressed == MAX) ? -1 : minPressed;
+#endif
 	}
 };
 
+#ifdef TEXTBOOK //doesn't work
+class clocksync_textbook : public clocksync
+{
+private:
+	std::size_t search(const std::size_t cur_switch)
+	{
+		if(cur_switch == nSwitch)
+		{
+			return allReachAt12() ? HIT : MAX;
+		}
+		
+		std::size_t ret = MAX;
+
+		for(int cnt = 0; cnt < 4; ++cnt)
+		{
+			ret = std::min(ret, cnt + search(cur_switch + 1));
+			pressSwitch(cur_switch);
+		}
+
+		return ret;
+	}
+};
+#endif
 
 int main(void)
 {
@@ -171,10 +207,14 @@ int main(void)
 	unsigned int input = 0;
 	std::unique_ptr<int[]> result(new int[total]);
 
-	clocksync cs;
+#ifdef TEXTBOOK
+	std::unique_ptr<clocksync> cs(new clocksync_textbook);
+#else
+	std::unique_ptr<clocksync> cs(new clocksync);
+#endif	
 	for(i = 0; i < total; i++)
 	{
-		result[i] = cs.calc();
+		result[i] = cs->calc();
 	}
 
     for(i = 0; i< total; i++)
