28347b74f4eca6b2708355b08adc6bea36240025
sedman <sedman@sedman.sedman>
2015-01-14 00:33:55 +0900

    dictionary : passed 41ms

--- 9.algospot_dictionary/algospot_dictionary.sedman.cpp
+++ 9.algospot_dictionary/algospot_dictionary.sedman.cpp
@@ -4,6 +4,16 @@
 #include <vector>
 #include <cstdio>
 
+/* TODO : 
+*/
+
+/* [brief]
+ * dictionary
+ * https://algospot.com/judge/problem/read/DICTIONARY
+ * performance : 41ms 
+ * etc : c++11
+ */
+
 class dictionary
 {
 private:	
@@ -18,31 +28,87 @@ private:
 		visited[here] = true;
 
 		//printf("++(%c)\n", (here + 'a'));
+
 		for(int to = 0; to < 26; ++to)
 		{
 			if(adj[here][to] == 1)
 			{
+				//printf("++1(%c)(%c)\n", (here + 'a'), (to + 'a'));
 				if(visited[to] == false)
 				{		
+					//printf("++2(%c)(%c)\n", (here + 'a'), (to + 'a'));
 					dfs(to);		
 				}
 			}
 		}
 
 		order.push_back(here);
-		printf("--(%c)\n", (here + 'a'));
+		//printf("--(%c)\n", (here + 'a'));
+	}
+
+	void makeGraph(const std::vector<std::string> &words)
+	{
+		for(auto &item : adj)
+		{
+			item.fill(0);
+		}
+
+		int cur = 1,prev = 0;
+		for(; cur < words.size(); ++cur)
+		{
+			prev = cur - 1;	
+			int minlength = std::min(words[prev].length(), words[cur].length());
+
+			for(int i = 0; i < minlength; ++i)
+			{
+				if(words[prev][i] != words[cur][i])
+				{
+					int from = words[prev][i] - 'a';
+					int to = words[cur][i] - 'a';
+					adj[to][from] = 1;			
+					break;
+				}
+			}
+		}
+	}
+
+	void calldfs(void)
+	{
+		//call dfs
+		visited.fill(false);
+		order.clear();
+		for(int i = 0; i < adj.size(); ++i)
+		{
+			if(visited[i] == false)
+			{
+				dfs(i);
+			}
+		}
+	}
+
+	bool findInvalid(void)
+	{
+		int i = 0, j = 0;
+		for(i = 0; i < adj.size(); ++i)
+		{
+			for(j = i + 1; j < adj.size(); ++j)
+			{
+				if(adj[order[i]][order[j]])
+				{
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 
 public:
 	std::string calc(void)
 	{
+		//prepare input
 		int nWord;
 		std::cin>>nWord;
-
-		for(auto &item : adj)
-		{
-			item.fill(0);
-		}
 
 		std::vector<std::string> words;
 		int maxlength = 0;
@@ -61,33 +127,9 @@ public:
 				
 		}
 
-/*
-		std::cout<<"\n\nmax len : "<<maxlength<<"\n";
-
-		std::for_each(words.begin(), words.end(), [](const std::string &str) 
-		{
-			std::cout<<str<<" ";
-		});
-*/
-		//make graph
-		int cur = 1,prev = 0;
-		for(; cur < nWord; ++cur)
-		{
-			prev = cur - 1;	
-			int minlength = std::min(words[prev].length(), words[cur].length());
-
-			for(int i = 0; i < minlength; ++i)
-			{
-				if(words[prev][i] != words[cur][i])
-				{
-					int from = words[prev][i] - 'a';
-					int to = words[cur][i] - 'a';
-					adj[from][to] = 1;			
-					break;
-				}
-			}
-		}
-/*
+		makeGraph(words);
+		
+#ifdef DEBUG
 		std::cout<<"\n";
 		for(const auto item : adj)
 		{
@@ -97,40 +139,24 @@ public:
 			}
 			std::cout<<"\n";
 		}
-*/
-		//
-		visited.fill(false);
-		order.clear();
-		int j = 0;
-		for(i = 0; i < adj.size(); ++i)
+#endif	
+		
+		calldfs();	
+		
+		if(findInvalid() == true)
 		{
-			//for(; j < adj.size(); ++j)
-			{
-				//if(adj[i][j] == 1 && 
-				if(visited[i] == false)
-				{
-					dfs(i);
-				}
-			}
-
-			std::cout<<"**";
-		}
-	
-		std::reverse(order.begin(), order.end());
-
-		//
-		for(i = 0; i < adj.size(); ++i)
-		{
-			for(j = i + 1; j < adj.size(); ++j)
-			{
-				if(adj[order[j]][order[i]])
-				{
-					return std::string("INVALID HYPOTHESIS");
-				}
-			}
+			return std::string("INVALID HYPOTHESIS");
 		}
 
-		return std::string("");
+		//make one string
+		std::string result;
+		std::for_each(order.begin(), order.end(), [&result](const int data) 
+		{
+			char ch = static_cast<char>(data + 'a');
+			result.push_back(ch);
+		});
+
+		return result;
 	}
 };
 
@@ -149,7 +175,7 @@ int main(void)
 		result[i] = dic.calc();
 	}
 
-    for(i = 0; i< total; i++)
+	for(i = 0; i< total; i++)
 	{	
 		std::cout<<result[i]<<std::endl;
 	}
